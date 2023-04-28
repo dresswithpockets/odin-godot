f1f1f7deb3502b2cfabe44465748d7379d199600
Ashley <dresswithpockets@pm.me>
2023-04-28 03:53:54 -0700

    show build timings

--- bindgen.sh
+++ bindgen.sh
@@ -1,2 +1,2 @@
-odin run ./bindgen -out:bindgen/bindgen.exe -vet -strict-style-init-only -debug -- ./godot-cpp/gdextension/extension_api.json
+odin run ./bindgen -out:bindgen/bindgen.exe -show-timings -vet -strict-style-init-only -debug -- ./godot-cpp/gdextension/extension_api.json
 
