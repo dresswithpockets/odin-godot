cf251f873a360f0d32836e2bb1dd8e8f37b32f90
Ashley <dresswithpockets@pm.me>
2023-05-09 06:12:32 -0700

    build and run godin with example project

--- example.sh
+++ example.sh
@@ -1,1 +1,3 @@
-odin build ./example_project/src -define:BUILD_CONFIG=float_64 -vet -build-mode:shared -out:example_project/bin/example.windows.x86_64.dll -warnings-as-errors -default-to-nil-allocator -target:windows_amd64 -debug
+odin build ./godin -vet -out:./godin/godin.exe -warnings-as-errors
+./godin/godin.exe build ./example_project/src -godot-import-path:../../
+odin build ./example_project/src -define:BUILD_CONFIG=float_64 -vet -build-mode:shared -out:./example_project/bin/example.windows.x86_64.dll -warnings-as-errors -default-to-nil-allocator -target:windows_amd64 -debug
