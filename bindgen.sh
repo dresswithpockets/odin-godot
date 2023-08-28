e9e12452c594031b02caa33fa78fb5176245e327
Ashley <dresswithpockets@pm.me>
2023-08-28 06:42:25 -0700

    fix scripts to use newer odin cli

--- bindgen.sh
+++ bindgen.sh
@@ -1,2 +1,2 @@
-odin run ./bindgen -out:bindgen/bindgen.exe -o:speed -show-timings -vet -strict-style-init-only -- ./godot-cpp/gdextension/extension_api.json
+odin run ./bindgen -out:bindgen/bindgen.exe -o:speed -show-timings -- ./godot-cpp/gdextension/extension_api.json
 
