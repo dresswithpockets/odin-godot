62c9c7967df11d759546c4076239676e8cf44af4
Ashley <dresswithpockets@pm.me>
2024-02-26 16:30:06 -0700

    remove unnecessary build scripts

--- bindgen.sh
+++
@@ -1,2 +0,0 @@
-odin run ./bindgen -out:bindgen/bindgen.exe -o:speed -show-timings -- ./godot-cpp/gdextension/extension_api.json
-
--- temple.sh
+++
@@ -1,1 +0,0 @@
-odin run temple/cli -o:speed -out:temple_cli.exe -- godin/templates temple
