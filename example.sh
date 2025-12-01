332fc260f68f495bc9f3de51bf3e9ba285f6dff3
CyberneticDruid <48985111+CyberneticDruid@users.noreply.github.com>
2025-11-30 22:20:29 -0700

    delete example.sh

--- example.sh
+++
@@ -1,3 +0,0 @@
-odin build ./godin -vet -out:./godin/godin.exe -warnings-as-errors
-./godin/godin.exe build ./example_project/src -godot-import-path:../../
-odin build ./example_project/src -define:BUILD_CONFIG=float_64 -vet -build-mode:shared -out:./example_project/bin/example.windows.x86_64.dll -warnings-as-errors -default-to-nil-allocator -target:windows_amd64 -debug
